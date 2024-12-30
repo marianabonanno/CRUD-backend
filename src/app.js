02ad8dc6f8c89f6d945e974995cd883fd396d6c0
mariana bonanno <marianabonanno98@hotmail.com>
2024-12-30 10:29:07 -0300

    fix commit 3

--- src/app.js
+++ src/app.js
@@ -4,32 +4,32 @@ import authRoutes from './routes/auth.routes.js';
 import Turnos from './routes/turnos.routes.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
+
+const app = express();
+
+
+app.use(cors({
+  origin: '*'  
+}));
+app.use(morgan('dev'));  
+app.use(express.json());  
+
+
 app.use('/turnos', Turnos);  
-app.use('/auth', authRoutes);     
+app.use('/auth', authRoutes);  
 
 
 app.get('/', (req, res) => {
   res.send('Servidor funcionando correctamente');
 });
 
+
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const app = express();
-
-app.use(cors({
-  origin: '*'
-}));
-app.use(morgan('dev'));
-app.use(express.json());
-
-
-app.use(authRoutes);
-
-app.use(Turnos);
 
 export default app;
