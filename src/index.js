7e83fcd08dd83dc2614cc27ae8ba9d4a3b2f885b
mariana bonanno <marianabonanno98@hotmail.com>
2024-12-30 10:53:07 -0300

    6to commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import app from './app.js'
 import {connectDB} from './db.js'
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
