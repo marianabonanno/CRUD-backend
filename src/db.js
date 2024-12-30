c6a4a26699c4df7d4aaaa2c0c431461376515ccc
mariana bonanno <marianabonanno98@hotmail.com>
2024-12-30 15:22:01 -0300

    commit 22

--- src/db.js
+++ src/db.js
@@ -5,9 +5,12 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI); 
-        console.log('DB esta conectada');
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('DB está conectada');
     } catch (error) {
-        console.log(error);
+      console.error('Error al conectar la base de datos:', error.message);
     }
-};
+  };
