db9a74bc7cf84e4ddca3dba2b546a93167f6d95e
mariana bonanno <marianabonanno98@hotmail.com>
2024-12-30 11:28:29 -0300

    octavo commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-import app from './index.js'; 
-const PORT = process.env.PORT || 10000;
+import express from 'express';
+import morgan from 'morgan';
+import authRoutes from './routes/auth.routes.js';
+import Turnos from './routes/turnos.routes.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: '*',
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/turnos', Turnos);
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
 });
+
+export default app; 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-import express from 'express';
-import morgan from 'morgan';
-import authRoutes from './routes/auth.routes.js';
-import Turnos from './routes/turnos.routes.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import app from './index.js';
 
-dotenv.config();
-
-const app = express();
-
-app.use(cors({
-  origin: '*',
-}));
-app.use(morgan('dev'));
-app.use(express.json());
-
-app.use('/turnos', Turnos);
-app.use('/auth', authRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando correctamente');
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
-export default app; 
