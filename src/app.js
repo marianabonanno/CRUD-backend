97ab68d891fab55e96d4ff77597a0ddffbe671fd
mariana bonanno <marianabonanno98@hotmail.com>
2024-12-30 16:29:48 -0300

    commit 24

--- src/app.js
+++ src/app.js
@@ -11,9 +11,20 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, () => {
-  console.log("Mongo connected");
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB conectado');
+  } catch (error) {
+    console.error('Error al conectar MongoDB:', error.message);
+  
+  }
+};
+
+connectDB();
 
 const allowedOrigins = [
   'https://merry-souffle-f4fa04.netlify.app',
